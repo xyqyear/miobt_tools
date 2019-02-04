c8b39129cb39e19dee89cd624dc396bb874d3a20
xyqyear <215908558@qq.com>
2019-02-04 09:43:09 +0800

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-# 待补全
+## 简介
+本程序用于定时自动从[miobt](http://miobt.com)获取番剧信息并自动创建transmission任务下载\
 
-有空补一下README,现在没啥时间(
+**注意：**\
+此程序的使用需要一定的Python/Json知识
+
+
+
+## 安装运行环境
+### 1.安装Python3.6+
+1.安装python：
+略
+2.安装依赖库：
+
+	pip install bcoding schedule requests transmissionrpc
+
+
+### 2.安装transmission
+**Linux：**
+1.Ubuntu/Debian/Raspbian/其他debian系系统：
+
+	apt-get install transmission-daemon
+
+2.RedHat/CentOS/其他redhat系系统：
+
+_未测试_
+
+**Windows:**
+
+_未测试_
+
+### 3.修改transmission配置文件
+
+修改“download-dir”为下载文件保存文件夹\
+确保“rpc-enabled”值为true
+
+### 4.修改config.py
+
+修改dowbload_path为刚刚在transmission配置文件中设置的路径\
+修改ip_address为安装了transmission机器的地址，如果是本机请填'127.0.0.1'\
+最后按照范例修改anime_list
+
+关于auto_copy和auto_remove:\
+auto_copy代表每个番下完后是否自动复制到独立设置的番剧储存目录\
+auto_remove代表是否在种子做种完成后删除相应的文件 ***如果auto_copy为False的话，请务必使此变量也为False***
+
+### 运行auto_download.py
+
+请保证此程序一直运行。
--- config.py
+++ config.py
@@ -10,7 +10,7 @@ anime_list = {}
 # 下面是范例
 # anime_list = {
 #     '兔女郎': {
-#         'preference': ['c.c动漫', 'c-a Raws'],  #这个是字幕组的偏向,访问 miobt.com 就知道应该怎么选啦,如果留为[]就是选最近的
+#         'preference': ['c.c动漫', 'c-a Raws'],  #这个是字幕组的偏向,排到越前面的越优先,访问 miobt.com 就知道应该怎么选啦,如果留为[]就是选最近的
 #         'date': 'thursday',
 #         'time': '03:00',
 #         'path': '/nfs/Own/ACGN/Anime/青春猪'},
@@ -27,8 +27,9 @@ anime_list = {}
 # }
 
 download_path = ''  # 这是transmission的下载目录,要和transmission的下载目录设置一致
-ip_address = '192.168.10.125' # transmission rpc的地址(默认值只是个例子)
-port = 9091 # 这个一般留默认
+# 例:download_path = 'D:\\Downloads' 或者 download_path = '/home/user/downloads'
+ip_address = '127.0.0.1'  # transmission rpc的地址(默认值是本机)
+port = 9091  # 这个一般留默认
 
 # 这个是transmission rpc的账号密码,没有设置可以留空
 username = ''
@@ -37,6 +38,6 @@ password = ''
 # 是否是debug模式
 debug_mode = False
 
-auto_copy = True
+auto_copy = False
 
-auto_remove = True
+auto_remove = False
